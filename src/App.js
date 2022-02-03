09bde6bddec93cf3091032270658660a2942bcbe
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-03 20:47:43 +0100

    added PersonalInformation at App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import PersonalInformation from "./components/PersonalInformation/PersonalInformation";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="title">FORMULARIOS</h1>
+      <PersonalInformation />
+    </div>
+  );
 }
 
 export default App;
